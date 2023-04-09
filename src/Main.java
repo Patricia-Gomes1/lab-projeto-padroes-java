4aa880b8ba29b565bf151102542448ddf91b79e1
Patricia-Gomes1 <patricia.gomes1@hotmail.com>
2023-04-09 13:43:03 -0300

    padrao strategy

--- src/Main.java
+++ src/Main.java
@@ -32,23 +32,25 @@ public class Main {
 
         // Strategy
 
-        Comportamento defensivo = new ComportamentoDefensivo();
-        Comportamento normal = new ComportamentoNormal();
-        Comportamento agressivo = new ComportamentoAgresivo();
-
-        Robo robo = new Robo();
-
-        robo.setComportamento(normal);
-        robo.mover();
-        robo.mover();
-
-        robo.setComportamento(defensivo);
-        robo.mover();
-
-        robo.setComportamento(agressivo);
-        robo.mover();
-        robo.mover();
-        robo.mover();
+        /*
+         * Comportamento defensivo = new ComportamentoDefensivo();
+         * Comportamento normal = new ComportamentoNormal();
+         * Comportamento agressivo = new ComportamentoAgresivo();
+         * 
+         * Robo robo = new Robo();
+         * 
+         * robo.setComportamento(normal);
+         * robo.mover();
+         * robo.mover();
+         * 
+         * robo.setComportamento(defensivo);
+         * robo.mover();
+         * 
+         * robo.setComportamento(agressivo);
+         * robo.mover();
+         * robo.mover();
+         * robo.mover();
+         */
 
     }
 }
